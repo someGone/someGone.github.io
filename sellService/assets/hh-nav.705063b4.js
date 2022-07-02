35aba5200587fa5c5781554fff53803cd6ff95fb
someGone <106517255+someGone@users.noreply.github.com>
2022-07-02 12:34:46 +0800

    Delete sellService directory

--- sellService/assets/hh-nav.705063b4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,k as t}from"./index.4f81b99f.js";const a={props:{text:{type:String,default:"\u9996\u9875"},isblack:{type:Boolean,default:!1},issearch:{type:Boolean,default:!0}},methods:{blackclick(){this.$emit("blackclick")},searchclick(){this.$emit("searchclick")}}};function c(r,s,_,o,i,l){return t("",!0)}var n=e(a,[["render",c],["__scopeId","data-v-68975a5f"]]);export{n as N};
